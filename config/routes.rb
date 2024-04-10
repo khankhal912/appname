ab0cc301d93852f3e3afd8b6381568a659dce19e
Khankhal_Dhaval <96652391+khankhal912@users.noreply.github.com>
2024-04-10 12:21:56 +0530

    Polymorphic Associations and Associations demo and learn.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :books
+  resources :authors
+  resources :accounts
+  resources :suppliers
   # get 'user/index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
